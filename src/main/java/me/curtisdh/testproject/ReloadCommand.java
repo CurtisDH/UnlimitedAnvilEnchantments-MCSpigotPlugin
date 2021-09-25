073587c4d69184657a8a113641ce5c373f3ab2f4
CurtisDH <39945904+CurtisDH@users.noreply.github.com>
2021-09-26 03:08:36 +1000

    - Added message to indicate a successful config refresh

--- src/main/java/me/curtisdh/testproject/ReloadCommand.java
+++ src/main/java/me/curtisdh/testproject/ReloadCommand.java
@@ -1,5 +1,6 @@
 package me.curtisdh.testproject;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -11,6 +12,7 @@ public class ReloadCommand implements CommandExecutor
         TestProject.PrintWithClassName(this, "Reloading config");
         TestProject.Instance.reloadConfig();
         TestProject.Instance.LoadConfig();
+        sender.sendMessage(ChatColor.GREEN+"Successfully reloaded config");
         return true;
     }
 }
