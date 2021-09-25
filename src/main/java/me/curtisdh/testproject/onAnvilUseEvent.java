d11c719f729065ee9759180ae457815cb00ab8a2
CurtisDH <39945904+CurtisDH@users.noreply.github.com>
2021-09-25 23:46:45 +1000

    - Commented debugging lines to prevent console spam

--- src/main/java/me/curtisdh/testproject/onAnvilUseEvent.java
+++ src/main/java/me/curtisdh/testproject/onAnvilUseEvent.java
@@ -15,64 +15,63 @@ public class onAnvilUseEvent implements Listener
     public static int requiredXpMultiplier = 7;
     public static int maxEnchantmentLevel = 10;
     public static int maxCost = 150;
+
     @EventHandler
     void AnvilEvent(PrepareAnvilEvent event)
     {
 
         AnvilInventory anvilInventory = event.getInventory();
-        String renameText = anvilInventory.getRenameText();
-        TestProject.PrintWithClassName(this, "RenameText:" + renameText);
+//        String renameText = anvilInventory.getRenameText();
+//        TestProject.PrintWithClassName(this, "RenameText:" + renameText);
         if (anvilInventory.getItem(0) != null && anvilInventory.getItem(1) != null)
         {
             Material item1Mat = anvilInventory.getItem(0).getType();
             Material item2Mat = anvilInventory.getItem(1).getType();
-            TestProject.PrintWithClassName(this,item1Mat.toString());
+            //TestProject.PrintWithClassName(this, item1Mat.toString());
             if (item1Mat != item2Mat)
             {
                 return;
             }
-            if(item1Mat == item2Mat)
+            if (item1Mat == item2Mat)
             {
-                if(item1Mat.toString().equalsIgnoreCase("ENCHANTED_BOOK"))
+                if (item1Mat.toString().equalsIgnoreCase("ENCHANTED_BOOK"))
                 {
                     return;
                 }
             }
 
             Map<Enchantment, Integer> item1Enchants = anvilInventory.getItem(0).getEnchantments();
-            TestProject.PrintWithClassName(this,item1Enchants.toString());
+            //TestProject.PrintWithClassName(this, item1Enchants.toString());
             Map<Enchantment, Integer> item2Enchants = anvilInventory.getItem(1).getEnchantments();
-            Map<Enchantment,Integer> newEnchantments = new HashMap<>();
+            Map<Enchantment, Integer> newEnchantments = new HashMap<>();
             //Copy all the enchantments to a new map
-            for(Map.Entry<Enchantment, Integer> all: item1Enchants.entrySet())
+            for (Map.Entry<Enchantment, Integer> all : item1Enchants.entrySet())
             {
                 newEnchantments.put(all.getKey(), all.getValue());
             }
             //check if they have the same enchantment, if so add one to the enchantment
-            for(Map.Entry<Enchantment, Integer> all: item2Enchants.entrySet())
+            for (Map.Entry<Enchantment, Integer> all : item2Enchants.entrySet())
             {
-                if(newEnchantments.containsKey(all.getKey()))
+                if (newEnchantments.containsKey(all.getKey()))
                 {
                     //only adds if matching currently, this seems to be okay for balancing aswell
-                    if(newEnchantments.get(all.getKey()) == all.getValue() )
+                    if (newEnchantments.get(all.getKey()) == all.getValue())
                     {
-                        if(all.getValue() + 1 > maxEnchantmentLevel)
+                        if (all.getValue() + 1 > maxEnchantmentLevel)
                         {
-                            newEnchantments.put(all.getKey(),all.getValue());
+                            newEnchantments.put(all.getKey(), all.getValue());
                             anvilInventory.setMaximumRepairCost(maxCost);
                             anvilInventory.setRepairCost(maxCost);
-                        }
-                        else
+                        } else
                         {
-                            newEnchantments.put(all.getKey(),all.getValue() + 1);
-                            anvilInventory.setMaximumRepairCost(((all.getValue()+1)*requiredXpMultiplier)*2);
-                            anvilInventory.setRepairCost((all.getValue()+1)*requiredXpMultiplier);
+                            newEnchantments.put(all.getKey(), all.getValue() + 1);
+                            anvilInventory.setMaximumRepairCost(((all.getValue() + 1) * requiredXpMultiplier) * 2);
+                            anvilInventory.setRepairCost((all.getValue() + 1) * requiredXpMultiplier);
                         }
                     }
-                }
-                else // doesnt contain the current enchantment so lets add it
+                } else // doesnt contain the current enchantment so lets add it
                 {
-                    newEnchantments.put(all.getKey(),all.getValue());
+                    newEnchantments.put(all.getKey(), all.getValue());
                 }
             }
             //create a new item based on the previously acquired type
