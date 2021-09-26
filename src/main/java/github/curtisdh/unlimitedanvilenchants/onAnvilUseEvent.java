562b70eea60408fe260461479bef05864fb8297e
CurtisDH <39945904+CurtisDH@users.noreply.github.com>
2021-09-27 00:27:48 +1000

    - Fixed bug when renaming result in Anvil | Slight implementation of enchanting books past max

--- src/main/java/github/curtisdh/unlimitedanvilenchants/onAnvilUseEvent.java
+++ src/main/java/github/curtisdh/unlimitedanvilenchants/onAnvilUseEvent.java
@@ -2,11 +2,14 @@ package github.curtisdh.unlimitedanvilenchants;
 
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.PrepareAnvilEvent;
 import org.bukkit.inventory.AnvilInventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.EnchantmentStorageMeta;
+import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.*;
 
@@ -21,39 +24,71 @@ public class onAnvilUseEvent implements Listener
     {
 
         AnvilInventory anvilInventory = event.getInventory();
-//        String renameText = anvilInventory.getRenameText();
-//        TestProject.PrintWithClassName(this, "RenameText:" + renameText);
+        Boolean itemIsBook = false;
+        //ItemMeta bookMetaData = null;
+        Map<Enchantment, Integer> item1Enchants = new HashMap<>();
+        Map<Enchantment, Integer> item2Enchants = new HashMap<>();
+
         if (anvilInventory.getItem(0) != null && anvilInventory.getItem(1) != null)
         {
             Material item1Mat = anvilInventory.getItem(0).getType();
             Material item2Mat = anvilInventory.getItem(1).getType();
             //TestProject.PrintWithClassName(this, item1Mat.toString());
-            if (item1Mat != item2Mat)
+            if (item1Mat != item2Mat && !item2Mat.toString().equalsIgnoreCase("ENCHANTED_BOOK")) //TODO rework this to allow books to enchant
             {
+                UnlimitedAnvilEnchants.PrintWithClassName(this, "HELLO");
                 return;
             }
-            if (item1Mat == item2Mat)
-            {
-                if (item1Mat.toString().equalsIgnoreCase("ENCHANTED_BOOK"))
-                {
-                    return;
-                }
-            }
 
-            Map<Enchantment, Integer> item1Enchants = anvilInventory.getItem(0).getEnchantments();
-            //TestProject.PrintWithClassName(this, item1Enchants.toString());
-            Map<Enchantment, Integer> item2Enchants = anvilInventory.getItem(1).getEnchantments();
+            //getEnchantments doesnt work on enchanted books for whatever reason
+            if (IsItemEnchantedBook(item1Mat))
+            {
+                EnchantmentStorageMeta itemMeta1 = (EnchantmentStorageMeta) anvilInventory.getItem(0).getItemMeta();
+                item1Enchants = itemMeta1.getStoredEnchants();
+                //Cant take books out of the anvil. I think it has something to do with the meta data but this doesnt work.
+//                for(Map.Entry<Enchantment,Integer> enchants: item1Enchants.entrySet())
+//                {
+//                   itemMeta1.removeStoredEnchant(enchants.getKey());
+//                }
+                //bookMetaData = itemMeta1;
+                if(item1Enchants.isEmpty())
+                {
+                    item1Enchants = anvilInventory.getItem(0).getEnchantments();
+                }
+                System.out.println(item1Enchants);
+            } else
+            {
+                item1Enchants = anvilInventory.getItem(0).getEnchantments();
+            }
+            if (IsItemEnchantedBook(item2Mat))
+            {
+                EnchantmentStorageMeta itemMeta2 = (EnchantmentStorageMeta) anvilInventory.getItem(1).getItemMeta();
+                item2Enchants = itemMeta2.getStoredEnchants();
+                if(item2Enchants.isEmpty())
+                {
+                    item2Enchants = anvilInventory.getItem(1).getEnchantments();
+                }
+                System.out.println(item2Enchants);
+
+            } else
+            {
+                item2Enchants = anvilInventory.getItem(1).getEnchantments();
+            }
             Map<Enchantment, Integer> newEnchantments = new HashMap<>();
             //Copy all the enchantments to a new map
             for (Map.Entry<Enchantment, Integer> all : item1Enchants.entrySet())
             {
+                System.out.println("Item1Enchants");
                 newEnchantments.put(all.getKey(), all.getValue());
             }
             //check if they have the same enchantment, if so add one to the enchantment
             for (Map.Entry<Enchantment, Integer> all : item2Enchants.entrySet())
             {
+                System.out.println("Item2Enchants");
+
                 if (newEnchantments.containsKey(all.getKey()))
                 {
+                    System.out.println("matching");
                     //only adds if matching currently, this seems to be okay for balancing aswell
                     if (newEnchantments.get(all.getKey()) == all.getValue())
                     {
@@ -76,13 +111,37 @@ public class onAnvilUseEvent implements Listener
             }
             //create a new item based on the previously acquired type
             ItemStack resultingItem = new ItemStack(item1Mat);
+
+            //Testing data to see if we can use the book if we maintain the NBT
+            //It does work but only for the original enchantment..
+//            if(bookMetaData != null)
+//            {
+//                resultingItem.setItemMeta(bookMetaData);
+//            }
+
+            //Rename the item to whatever the user inputs.
+            String renameText = event.getInventory().getRenameText();
+            if (!(renameText.isEmpty()))
+            {
+                ItemMeta m = resultingItem.getItemMeta();
+                m.setDisplayName(renameText);
+                resultingItem.setItemMeta(m);
+            }
+            //System.out.println(resultingItem.getItemMeta());
+
             //enchant the newly created item
             resultingItem.addUnsafeEnchantments(newEnchantments);
             //add the new item to the anvil result
-
             event.setResult(resultingItem);
-
-
         }
+    }
+
+    private boolean IsItemEnchantedBook(Material itemMaterial)
+    {
+        if (itemMaterial.toString().equalsIgnoreCase("ENCHANTED_BOOK"))
+        {
+            return true;
+        }
+        return false;
     }
 }
